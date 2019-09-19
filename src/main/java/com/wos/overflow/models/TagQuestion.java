5084ab2b1e9f5dc1106e3b561a305361fe48f7a4
Rafael Tongson <rtongson@peppymiint.local>
2019-09-18 18:24:05 -0700

    getters and setters make for TagQuestion table model

--- src/main/java/com/wos/overflow/models/TagQuestion.java
+++ src/main/java/com/wos/overflow/models/TagQuestion.java
@@ -1,10 +1,102 @@
 package com.wos.overflow.models;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="tags_questions")
 public class TagQuestion {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	// Dates
+	@Column(updatable=false)
+    private Date createdAt;
+    private Date updatedAt;
+    
+	// // // // // // // // //
+	// relationship mapping
+    
+    // Establish relationship with a question
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
+    private Question question;
+    
+    // Establish relationship with a tag
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
+    private Tag tag;
+    
+    
+    
+    public TagQuestion() {
+    	// constructor
+    }
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	public Tag getTag() {
+		return tag;
+	}
+
+	public void setTag(Tag tag) {
+		this.tag = tag;
+	}
+    
+	// // // // // //
+	// PERSISTENCE 
+	// // runs the method right before the object is created
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    // // runs a method when the object is modified
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+    //END MODEL
+    
 }
